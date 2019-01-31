8ef8151c06bebdd84e0ae8e3d79822732578862d
Jeevan Bains <harjeevanbains@gmail.com>
2019-01-31 11:42:42 -0800

    fixed coding style in model classes

--- app/Models/students.php
+++ app/Models/students.php
@@ -2,22 +2,22 @@
 use CodeIgniter\Model;
 
 class students extends Model
-{ 
+{
 
-$data = [
-'Cassidy' => ['id'=>'a00986001', 'fullname' => 'Cassidy Banks', 'year'=> 2],
-'Malcolm' => ['id'=>'a00986002', 'fullname' => 'Malcolm Longboi', 'year'=> 3],
-'Ernie' => ['id'=>'a00986003', 'fullname' => 'Ernie Smallman', 'year'=>1]
-];
+    protected $data = [
+        'Cassidy' => ['id' => 'a00986001', 'fullname' => 'Cassidy Banks', 'year' => 2],
+        'Malcolm' => ['id' => 'a00986002', 'fullname' => 'Malcolm Longboi', 'year' => 3],
+        'Ernie' => ['id' => 'a00986003', 'fullname' => 'Ernie Smallman', 'year' => 1],
+    ];
 
+    public function findid($id = null)
+    {
+        return $this->data[$id];
+    }
 
-
-public findid($id=null){
-return $this->data[$id];
-}
-
-public findall(int $limit = 0, int $offset = 0){
-	return$this->data;
-}
+    public function findall(int $limit = 0, int $offset = 0)
+    {
+        return $this->data;
+    }
 
 }
